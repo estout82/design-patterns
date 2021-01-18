f5e1798edf61c7a937c16055c79565dd100fcd74
Eric Stoutenburg <eric.stoutenburg@baysideonline.com>
2021-01-18 15:03:19 -0800

    Removed private value

--- singleton/Instance.cs
+++ singleton/Instance.cs
@@ -4,16 +4,11 @@ namespace DesignPatterns.Singleton
 {
   public class Instance
   {
-    private string value;
-
-    public Instance(string value = "Default")
-    {
-      this.value = value;
-    }
+    public Instance() { }
 
     public string Value
     {
-      get { return value; }
+      get; set;
     }
   }
 }
